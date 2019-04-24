1efab959ea60e5954d8f0049719d1d80dc2245cf
Dongjin Kim <43647261+dongjinkim94@users.noreply.github.com>
2019-04-24 20:10:33 +0900

    20190424
    수업중ㅇ

--- 20190424 Practice/Lab05.py
+++ 20190424 Practice/Lab05.py
@@ -1,2 +1,7 @@
 # 20190424 Lab05
-#
+# 리스트의 기초연산
+
+marvel_heroes = ['스파이더맨', '헐크', '아이언맨']
+dc_heroes = ['슈퍼맨', '배트맨', '원더우먼']
+heroes = marvel_heroes + dc_heroes
+print(heroes)
---
+++ 20190424 Practice/Lab06.py
@@ -0,0 +1,6 @@
+# 20190424 Lab06
+# 요소 찾기
+
+heroes = ['스파이더맨','슈퍼맨','헐크','아이언맨','배트맨']
+index = heroes.index("슈퍼맨")
+print(index)
