bdd3d64ea67501c0db464e922688a0f79dd2f3e8
Dongjin Kim <43647261+dongjinkim94@users.noreply.github.com>
2019-05-08 21:07:32 +0900

    졸리다
    빨리 끝났으면 좋게삳........

--- 20190508/Lab03.py
+++ 20190508/Lab03.py
@@ -3,7 +3,7 @@
 # 파티에 참석한 사람들의 명단이 세트 A와 B애 각각 저장되어 있다.
 # 2개 파티에 모두 참석한 사람들의 명단을 출력하려면 어떻게 해야할까?
 
-partyA = set(["Park", "Kim", "Lee"])
+partyA = set(["Park", "Kim", "Lee"]) # 빈 리스트를 set으로 변환
 partyB = set(["Park", "Choi"])
 
 print("2개의 파티에 모두 참석한 사람은 다음과 같습니다.")
--- 20190508/Lab04.py
+++ 20190508/Lab04.py
@@ -1,1 +1,33 @@
 # 20190508 Lab4
+# 파일에서 중복되지 않은 단어의 갯수
+# 텍스트 파일을 읽어서 단어를 얼마나 다양하게 사용하여 문서를 작성하였는지를 계산하는
+# 프로그램을 작성해보자.
+
+# 참고 -> 파일 Input, Output
+# 파일을 연다.
+# fname = input("입력 파일 이름: ") # Lab04_proverbs
+# file = open(fname,"r") # r = read only, w = write
+# 파일의 모든 줄에 대하여 반복한다.
+# for line in file:
+#   lineWords = line.split()
+
+fname = input("입력 파일 이름: ")
+file = open(fname, "r")  # r = read only, w = write
+a = set([])
+for line in file:
+    # 단어 단위로 분리 -> 리스트 저장
+    lineWords = line.split()
+
+    # 리스트에 저장된 워드(단어)를 순차적으로 접근하면서 가져오는 것
+
+    for words in lineWords:
+        new_words = ""
+        # 새로운 워드: 구두점(' or .) 등을 제거 순수 문자 저장
+        # 문자 하나하나씩 접근
+        for ch in words:
+            # 알파벳(영문자) 저장
+            if ch.isalpha():
+                new_words += ch
+        a.add(new_words.lower())
+print(a)
+print("%d개의 단어(중복제외)" % (len(a)))
