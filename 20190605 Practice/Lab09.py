a939ba85edb207c6bedba9c7f890883db45b1042
Dongjin Kim <43647261+dongjinkim94@users.noreply.github.com>
2019-06-05 22:30:39 +0900

    끝
    끝

--- 20190605 Practice/Lab09.py
+++ 20190605 Practice/Lab09.py
@@ -1,11 +1,14 @@
 # 20190605 Lab09
 #
 
+import time
+
 N = int(input("2 이상의 자연수 입력 : "))
 while N <= 1:
     print("잘못입력하셨습니다.")
     N = int(input("2 이상의 자연수 입력 : "))
 
+start = time.time()
 a = list(range(N + 1))
 a[1] = 0
 i = 2
@@ -16,6 +19,8 @@ while i <= N / 2:
         a[i * j] = 0
         j += 1
     i += 1
+end = time.time() - start
+print("실행시간:",end)
 
 print("0이 아닌 a[i]의 값")
 for i in range(1, N + 1):
